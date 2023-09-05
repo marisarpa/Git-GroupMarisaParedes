1160aa68a613c9c0a9d953fe24d44bb9171a808a
Marisa Paredes <127986162+marisarpa@users.noreply.github.com>
2023-09-05 04:32:15 +0000

    ejercicio 1 y 2

---
+++ PythonB_listrooms.py
@@ -0,0 +1,10 @@
+import requests
+access_token = 'MjRkNjA3NzUtMjJmMi00YzQ2LWJmMWUtMjg3NDMxMzhjYmQ4M2Y0OTU5OWUtNDc2_P0A1_b7e3ec15-6d63-442e-9ee4-f9d3a59ed831'
+url = 'https://webexapis.com/v1/rooms'
+headers = {
+ 'Authorization': 'Bearer {}'.format(access_token),
+ 'Content-Type': 'application/json'
+}
+params={'max': '100'}
+res = requests.get(url, headers=headers, params=params)
+print(res.json())
